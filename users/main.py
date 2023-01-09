def51053621b89f723248c94ee575cc335d34b53
Ankit <ankit.goyal@zohocorp.com>
2023-01-09 17:03:47 +0530

    Check before performing CRUD based on Id

--- users/main.py
+++ users/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends, status, Response
 from users import schemas, models
 from users.database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from sqlalchemy import or_
 from uuid import uuid4
 
 app = FastAPI()
@@ -17,7 +18,7 @@ def get_db():
 
 @app.post("/users", status_code=status.HTTP_201_CREATED )
 def create_user(user: schemas.User, response: Response, db: Session = Depends(get_db)):
-    check_user = db.query(models.User).filter(models.User.registerNumber == user.registerNumber).first()
+    check_user = db.query(models.User).filter(or_(models.User.registerNumber == user.registerNumber, models.User.personalEmail == user.personalEmail, models.User.officialEmail == user.officialEmail)).first()
     if not check_user:
         user.id = uuid4().hex
         new_user = models.User(**user.dict())
@@ -26,12 +27,15 @@ def create_user(user: schemas.User, response: Response, db: Session = Depends(ge
         db.refresh(new_user)
         return {"data": new_user, "status": "success", "code": "SUCCESS"}
     response.status_code=status.HTTP_406_NOT_ACCEPTABLE
-    return {"data": f"User with register number {user.registerNumber} already exists", "status": "failure", "code": "FAILURE"}
+    return {"data": f"User already exists", "status": "failure", "code": "FAILURE"}
 
 @app.get("/users")
-def all_users(db: Session = Depends(get_db)):
+def all_users(response: Response, db: Session = Depends(get_db)):
     users = db.query(models.User).all()
-    return {"data": users, "count": users.__len__()}
+    if users.__len__():
+        return {"data": users, "count": users.__len__()}
+    response.status_code=status.HTTP_406_NOT_ACCEPTABLE
+    return {"data": "There are no users in the database", "count": 0}
 
 @app.get("/users/{id}")
 def get_user_by_id(id: str, response: Response, db: Session = Depends(get_db)):
@@ -55,11 +59,14 @@ def all_users(db: Session = Depends(get_db)):
     return {"data": "All users deleted"}
 
 @app.delete("/users/{id}")
-def delete_user_by_id(id: str, db: Session = Depends(get_db)):
-    db.query(models.User).filter(models.User.id == id).delete(synchronize_session=False)
-    db.commit()
-    return {"data": f"User with id {id} is deleted successfully"}
-
+def delete_user_by_id(id: str, response: Response, db: Session = Depends(get_db)):
+    current_user = db.query(models.User).filter(models.User.id == id)
+    if current_user.first():
+        current_user.delete(synchronize_session=False)
+        db.commit()
+        return {"data": f"User with id {id} is deleted successfully"}
+    response.status_code=status.HTTP_404_NOT_FOUND
+    return {"data": f"User with the id {id} doesn't exist"}
 
 if __name__ == "__main__":
     models.Base.metadata.create_all(engine)
